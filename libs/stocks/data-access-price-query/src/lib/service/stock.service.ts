3d2de2f59fe31b76d1df5cfede4d77f282e693f9
Arun <arunrj08@gmail.com>
2019-12-01 09:51:16 -0800

    Updated test for service

--- libs/stocks/data-access-price-query/src/lib/service/stock.service.ts
+++ libs/stocks/data-access-price-query/src/lib/service/stock.service.ts
@@ -1,23 +1,19 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {
-  StocksAppConfig,
-  StocksAppConfigToken
-} from '@coding-challenge/stocks/data-access-app-config';
 import { PriceQueryResponse } from './../+state/price-query.type';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockService {
 
-  getStockDetails(symbol, period){
+   getStockDetails(symbol, period): Observable<PriceQueryResponse[]>{
     const apiURL=  `/api/stock/${symbol}/chart/${period}`;
     return this.httpClient.get<PriceQueryResponse[]>(apiURL);
   }
 
   constructor(
-    @Inject(StocksAppConfigToken) private env: StocksAppConfig,
     private httpClient: HttpClient
   ) {}
 
