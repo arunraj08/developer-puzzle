75ccd6a05a89ad10838bd129667cfc9f2578a421
Arun <arunrj08@gmail.com>
2019-11-30 23:10:01 -0800

    Stock API route changes

--- apps/stocks-api/src/environments/environment.ts
+++ apps/stocks-api/src/environments/environment.ts
@@ -3,5 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'Tpk_b8f98519fd9046c0862c539e92d08ab1',
+  apiURL: 'https://sandbox.iexapis.com'
 };
--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -2,15 +2,31 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  **/
-import { Server } from 'hapi';
+const Hapi = require('@hapi/hapi');
+const Joi = require('@hapi/joi');
+const Wreck = require('@hapi/wreck');
 
+import { environment as env} from './environments/environment';
 const init = async () => {
-  const server = new Server({
+  const server = Hapi.server({
     port: 3333,
     host: 'localhost'
   });
 
-  server.route({
+  const getStock = async function( symbol, period) {
+    
+    const apiURL = `${env.apiURL}/beta/stock/${symbol}/chart/${period}?token=${env.apiKey}`;
+
+    console.log(apiURL);
+
+    const { res, payload } = await Wreck.get(apiURL, {
+      json: true
+    });
+
+    return payload;  
+  };
+
+  /* server.route({
     method: 'GET',
     path: '/',
     handler: (request, h) => {
@@ -18,6 +34,48 @@ const init = async () => {
         hello: 'world'
       };
     }
+  }); */
+
+  server.method({
+      name: 'stock',
+      method: getStock,
+      options: {
+        cache: {
+          expiresIn: 60 * 1000,
+          generateTimeout: 10000
+        },
+        generateKey: function (symbol, period){
+          const id = `${symbol}:${period}`;
+          console.log(id);
+          return id;
+        }
+      }  
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/api/stock/{symbol}/chart/{period?}',
+    handler: async (request, h) => {
+
+      const symbol = request.params.symbol;
+      const period = request.params.period;
+      console.log('symbol :',symbol, 'period :',period);
+
+      let stockData = await server.methods.stock(symbol, period);
+      
+      return h.response(stockData);
+
+    },
+
+    options: {
+      validate: {
+          params: Joi.object({
+              symbol: Joi.string().required(),
+              period: Joi.string().required()
+          })
+      }
+    }
+
   });
 
   await server.start();
