3b0e681f54c6aad6fce7802ff4e534ba3c79470f
Arun <arunrj08@gmail.com>
2019-12-01 19:12:41 -0800

    Updated Hapi code with plugin implementaion

--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -1,75 +1,20 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- **/
+import * as Glue from '@hapi/glue';
+import { manifest } from './app/config/manifest';
 
-import * as Hapi from '@hapi/hapi';
-import * as Joi from '@hapi/joi';
-import { getStock } from './app/service/stockService'
-
-const init = async () => {
-
-  const server = Hapi.server({
-    port: 3333,
-    host: 'localhost',
-    routes: {
-      cors: {
-        origin: ["*"],
-        headers: ["Accept", "Content-Type"]
-      }
-    }
-  });
-
-
-  server.method({
-      name: 'stock',
-      method: getStock,
-      options: {
-        cache: {
-          expiresIn: 60 * 1000,
-          generateTimeout: 10000
-        },
-        generateKey: function (symbol, period){
-          const id = `${symbol}:${period}`;
-          console.log(id);
-          return id;
-        }
-      }  
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/api/stock/{symbol}/chart/{period?}',
-    handler: async (request, h) => {
-
-      const symbol = request.params.symbol;
-      const period = request.params.period;
-      console.log('symbol :',symbol, 'period :',period);
-
-      let stockData = await server.methods.stock(symbol, period);
-      
-      return h.response(stockData);
-
-    },
-
-    options: {
-      validate: {
-          params: Joi.object({
-              symbol: Joi.string().required(),
-              period: Joi.string().required()
-          })
-      }
-    }
-
-  });
-
-  await server.start();
-  console.log('Server running on %s', server.info.uri);
+const options = {
+    relativeTo: __dirname
 };
 
-process.on('unhandledRejection', err => {
-  console.log(err);
-  process.exit(1);
-});
+const startServer = async function () {
+    try {
+        const server = await Glue.compose(manifest, options);
+        await server.start();
+        console.log('Server running on %s', server.info.uri);
+    }
+    catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
 
-init();
+startServer();
